b0aec169aa71012567801a26f57da6df59816498
Leanna Calla <leanna.calla@uoit.net>
2015-10-28 10:17:58 -0400

    commit done by Armin

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,14 @@
+reader = open ('gadsby_stripped.txt', 'r')
+line = reader.readline()
+total = 0
+count = 0
+while line != '':
+	count += 1
+	total += len(line)
+	line = reader.readeline()
+	def has_no_e (line):
+			for i in line:
+				print(e in line)
+				return(print(e in line))
+reader.close()
+		
---
+++ Midterm/strip.sh
@@ -0,0 +1,7 @@
+def input (filename, k m):
+	
+
+Leanna Calla 100555398
+
+for filename | head -n -m | rm
+for filename | tail -n -k | rm
